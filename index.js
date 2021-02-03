cef101131024d71af31e8d813a85639df6570b22
menachem margolis <margolismendel5728@gmail.com>
2021-02-02 19:25:49 -0500

    commit tree e98fd188beaf7bc5667664d3d2fb5f5fa9607e17

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ function handleClick(e) {
   displayMessage(greet(timeString))
 }
 
+function displayMessage(msg){
+  document.getElementById("greeting").innerHTML = msg
+}
+
+function greet(timStr){
+  let time = parseInt(timStr)
+
+  if (time < 12 ){return "Good Morning"}
+  else if (time > 17 ){return "Good Evening"}
+  else {return "Good Afternoon"}
+}
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
